9d47b6bf014654511a92a3a8bf49e4f16e11dcd2
zekeer21 <achas241@gmail.com>
2024-11-12 10:07:26 +0800

    Modified configs

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./Auth/login";
 import Signup from "./Auth/signup";
+import Hello from "./components/Hello";
 
 function App() {
   return (
+    <>
     <Router>
       <Routes>
+        <Route path="/" element={<Hello />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
        
       </Routes>
     </Router>
+    </>
+
   );
 }
 
