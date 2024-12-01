cd32b9fc370dec9f2a8235a2f02ad9d23b72c8ab
John Carlo E. Ladia <jcladia164@gmail.com>
2024-12-02 03:01:45 +0800

    I am done except sa merge

--- src/Artist/navbarArtist.jsx
+++ src/Artist/navbarArtist.jsx
@@ -26,7 +26,11 @@ export default function NavbarArtist({ fullName }) {
       <div className="flex items-center justify-between px-8 py-4">
         {/* Logo */}
         <div className="flex items-center">
-          <img src="/images/logo-blue.png" alt="LokalArt Logo" className="h-10" />
+          <img
+            src="/images/logo-blue.png"
+            alt="LokalArt Logo"
+            className="h-10"
+          />
         </div>
 
         {/* Hamburger Menu (Mobile) */}
@@ -41,17 +45,23 @@ export default function NavbarArtist({ fullName }) {
         <div className="hidden sm:flex flex-1 px-8 justify-center items-center">
           <ul className="flex justify-center space-x-14">
             <li>
-              <Link to="/artist/discover" className="text-black hover:text-cyan-500">
+              <Link
+                to="/artist/discover"
+                className="text-black hover:text-cyan-500"
+              >
                 Discover
               </Link>
             </li>
             <li>
-              <Link to="/artist/transactions" className="text-black hover:text-cyan-500">
+              <Link
+                to="/artist/transactions"
+                className="text-black hover:text-cyan-500"
+              >
                 My Transactions
               </Link>
             </li>
             <li>
-              <Link to="/messages" className="text-black hover:text-cyan-500">
+              <Link to="/message" className="text-black hover:text-cyan-500">
                 Messages
               </Link>
             </li>
@@ -61,7 +71,12 @@ export default function NavbarArtist({ fullName }) {
         {/* User Actions */}
         <div className="hidden sm:flex items-center space-x-6">
           <div className="cursor-pointer">
-            <Icon icon="mdi:bell" width="24" height="24" className="text-cyan-500" />
+            <Icon
+              icon="mdi:bell"
+              width="24"
+              height="24"
+              className="text-cyan-500"
+            />
           </div>
           <div className="w-8 h-8 border border-gray-300 rounded-full bg-[#ffffff] text-black flex items-center justify-center cursor-pointer">
             {getInitials(fullName)}
@@ -80,12 +95,18 @@ export default function NavbarArtist({ fullName }) {
         <div className="sm:hidden px-8 py-4 float-end">
           <ul className="space-y-4">
             <li>
-              <Link to="/client/discover" className="text-black hover:text-cyan-500">
+              <Link
+                to="/client/discover"
+                className="text-black hover:text-cyan-500"
+              >
                 Discover
               </Link>
             </li>
             <li>
-              <Link to="artist/transactions" className="text-black hover:text-cyan-500">
+              <Link
+                to="artist/transactions"
+                className="text-black hover:text-cyan-500"
+              >
                 My Transactions
               </Link>
             </li>
